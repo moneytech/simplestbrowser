656659d17c7ecc43082a2ae721eef35fda626f59
DefunctLizard <grantshandy@gmail.com>
2020-09-14 13:49:58 -0600

    finish

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,67 @@ use gtk::*;
 use gtk::prelude::*;
 use gio::prelude::*;
 use std::env::args;
-use webkit2gtk::{SettingsExt, WebContext, WebContextExt, WebView, WebViewExt};
+use webkit2gtk::{WebContext, WebView, WebViewExt};
 
 fn main() {
+
+//  Declare gtk::Application
     let application = gtk::Application::new(Some("org.skylinecc.SimplestBrowser"), Default::default())
         .expect("Initialization failed...");
 
     application.connect_activate(|app| {
 
-        // Load the compiled resource bundle
+//      Load the compiled resource bundle
         let resources_bytes = include_bytes!("../data/resources.gresource");
         let resource_data = glib::Bytes::from(&resources_bytes[..]);
         let res = gio::Resource::new_from_data(&resource_data).unwrap();
         gio::resources_register(&res);
 
-        // Load the window UI
+//      Load the window UI
         let builder = Builder::new_from_resource("/org/skylinecc/SimplestBrowser/window.ui");
 
-        // Get a reference to the window
-        let window: ApplicationWindow = builder.get_object("window").expect("Couldn't get window");
+//      Get a reference to the window
+        let window: gtk::ApplicationWindow = builder.get_object("window").expect("Couldn't get window");
         window.set_application(Some(app));
 
+//      Reference boxes and buttons for the window
         let window_box: gtk::Box = builder.get_object("window_box").unwrap();
-//        let forward_button: gtk::Button = builder.get_object("forward_button").unwrap();
+        let forward_button: gtk::Button = builder.get_object("forward_button").unwrap();
         let back_button: gtk::Button = builder.get_object("back_button").unwrap();
         let search_entry: gtk::Entry = builder.get_object("search_bar").unwrap();
 
+//      Declare Webview and WebContext
     	let context = WebContext::get_default().unwrap();
 		let webview = WebView::new_with_context(&context);
 
+//      Set basic Webview settings
 		webview.load_uri("https://duckduckgo.com/");
 		window_box.pack_start(&webview, true, true, 0);
 
-		let webview_clone1 = webview.clone();
-		let webview_clone2 = webview.clone();
+//      Clone webview for back & forward button actions :/
+		let webview_back = webview.clone();
+		let webview_forward = webview.clone();
 
+//      Search Entry handler, set URL as text in search_entry when <enter> is pressed.
 		search_entry.connect_activate(move |search_entry|{
 			let url = search_entry.get_text().unwrap();
 			webview.load_uri(&url);
 		});
 
-//		forward_button.connect_activate(move |forward_button| {
-//			let webview = webview.clone();
-//			webview.go_forward();
-//		});
+//      Go forwards and backwards pages when the buttons are clicked
+		forward_button.connect_clicked(move |_| {
+			webview_forward.go_forward();
+		});
 
-		back_button.connect_activate(move |_| {
-			let webview_clone1 = webview.clone();
-			webview.go_back();
+		back_button.connect_clicked(move |_| {
+			webview_back.go_back();
 		});
 
 
 
-        // Show the UI
+//      Show the UI and finish up
         window.show_all();
     });
 
     application.run(&args().collect::<Vec<_>>());
 }
-
-
-
-
